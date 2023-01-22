85ba8070634900926844142c35936a13ac6f9080
Daniel <danatkhoo@yahoo.com.br>
2023-01-22 11:36:28 -0300

    adding the ATF estimation plugin configuration step to setup script + more logs during script run

--- scripts/automation_setup.sh
+++ scripts/automation_setup.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
+echo "Setting up custom ATF-chrome-plugin"
+
+# setup of custom ATF-chrome-plugin
+mkdir ~/wptagent-automation/extensions
+git clone https://github.com/danielatk/ATF-chrome-plugin ~/wptagent-automation/extensions/ATF-chrome-plugin
+
+echo "Setting up custom WPTagent files"
+
 # using custom files
 python3 ~/wptagent-automation/scripts/setup_webpagetest_files.py
+
+echo "Installing and setting up golang"
 
 # install go
 sudo rm -rf /usr/bin/go
@@ -16,6 +26,8 @@ source ~/.profile
 
 cd ~/wptagent-automation/
 
+echo "Setting up NDT client"
+
 # build ndt client
 git clone https://github.com/m-lab/ndt7-client-go
 cd ndt7-client-go/cmd/ndt7-client
@@ -23,9 +35,13 @@ go mod tidy
 go build .
 cp ndt7-client ../../../scripts/ndt/
 
+echo "Installing chromedriver and necessary python packages"
+
 # install webdriver and other python packages
 sudo apt install -y chromium-chromedriver
 pip3 install pandas
+
+echo "Setting up puppeteer"
 
 # configure puppeteer
 cd ~/wptagent-automation/scripts/puppeteer/puppeteer_navigation
@@ -35,12 +51,16 @@ collectionServerUrl=$(cat ~/wptagent-automation/collection_server_url)
 collectionServerUser=$(cat ~/wptagent-automation/collection_server_user)
 collectionServerSshPort=$(cat ~/wptagent-automation/collection_server_ssh_port)
 
+echo "Setting up SSH"
+
 # configure ssh
 sudo apt install -y sshpass
 ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N ""
 sshpass -f ~/wptagent-automation/collection_server_password ssh-copy-id -i ~/.ssh/id_rsa -p $collectionServerSshPort $collectionServerUser@$collectionServerUrl
 sudo systemctl enable ssh
 sudo systemctl start ssh
+
+echo "Setting up cron jobs"
 
 # adding experiment scripts to crontab
 crontab -l > mycron
