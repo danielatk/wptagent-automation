802d7f61e1f8783b69826901206a5ba97b8a32df
Daniel <danatkhoo@yahoo.com.br>
2023-03-25 10:33:36 -0300

    changing ndt to measure from M-Lab servers as well as RNP servers

--- scripts/ndt/measure_ndt.sh
+++ scripts/ndt/measure_ndt.sh
@@ -2,6 +2,7 @@
 
 logFile="/home/pi/wptagent-automation/log_ndt"
 sampleUfFile="/home/pi/wptagent-automation/scripts/ndt/sample_from_uf_list.py"
+getServerFQDNFile="/home/pi/wptagent-automation/scripts/ndt/get_server_fqdn.py"
 
 collectionServerUrl=$(cat /home/pi/wptagent-automation/collection_server_url)
 collectionServerUser=$(cat /home/pi/wptagent-automation/collection_server_user)
@@ -11,8 +12,14 @@ uf="$(python3 $sampleUfFile)"
 dateStr="$(date +%s)"
 ndt_file_path="/home/pi/wptagent-automation/ndt_data/$(cat /home/pi/wptagent-automation/mac)_${dateStr}_${uf}_ndt.json"
 traceroute_file_path="/home/pi/wptagent-automation/ndt_data/$(cat /home/pi/wptagent-automation/mac)_${dateStr}_${uf}_traceroute"
-/home/pi/wptagent-automation/scripts/ndt/ndt7-client -server $uf.medidor.rnp.br:4443 -no-verify -scheme wss -format=json > $ndt_file_path
-traceroute $uf.medidor.rnp.br > $traceroute_file_path
+if [ "$uf" == "mlab" ]; then
+    /home/pi/wptagent-automation/scripts/ndt/ndt7-client -no-verify -scheme wss -format=json > $ndt_file_path
+    server_fqdn="$(python3 $getServerFQDNFile $ndt_file_path)"
+    traceroute $server_fqdn > $traceroute_file_path
+else
+    /home/pi/wptagent-automation/scripts/ndt/ndt7-client -server $uf.medidor.rnp.br:4443 -no-verify -scheme wss -format=json > $ndt_file_path
+    traceroute $uf.medidor.rnp.br > $traceroute_file_path
+fi
 echo "measure NDT -> test completed successfully" >> $logFile
 scp -o StrictHostKeyChecking=no -P $collectionServerSshPort $ndt_file_path $traceroute_file_path $collectionServerUser@$collectionServerUrl:~/wptagent-control/ndt_data/
 echo "measure NDT -> test data sent to collection server" >> $logFile
