8b1ba760a9e382d3c9df45d3aa51f999948621ea
cleitonmoya <cleitonmoya@gmail.com>
2023-05-05 23:28:30 -0300

    bbr script

--- scripts/network_setup.sh
+++ scripts/network_setup.sh
@@ -3,15 +3,19 @@
 # Habilitação do TCP BBR
 BRR=n
 read -p 'Habilitar TCP BBR? [y/n]' BBR
-if [ BBR = y]; then
-    sudo modprobe tcp_bbr
-    echo 'net.ipv4.tcp_congestion_control = bbr' | sudo tee -a /etc/sysctl.conf
-    echo 'net.core.default_qdisc = fq' | sudo tee -a /etc/sysctl.conf
-    sudo sysctl -p
-
-    if [ $(sysctl net.ipv4.tcp_congestion_control | sed 's/^.*= //') = bbr ]; then
-        echo 'TCP BBR habilitado com sucesso.'
+if [ $BBR = y ]; then
+    if [ $(sudo sysctl net.ipv4.tcp_congestion_control | sed 's/^.*= //') = bbr ]; then
+        echo 'TCP BBR já habilitado'
     else
-        echo 'Não foi possível habilitar o TCP BBR, por favor verifique.'
+        sudo modprobe tcp_bbr
+        echo 'net.ipv4.tcp_congestion_control = bbr' | sudo tee -a /etc/sysctl.conf
+        echo 'net.core.default_qdisc = fq' | sudo tee -a /etc/sysctl.conf
+        sudo sysctl -p
+
+        if [ $(sudo sysctl net.ipv4.tcp_congestion_control | sed 's/^.*= //') = bbr ]; then
+            echo 'TCP BBR habilitado com sucesso.'
+        else
+            echo 'Não foi possível habilitar o TCP BBR, por favor verifique.'
+        fi
     fi
 fi
