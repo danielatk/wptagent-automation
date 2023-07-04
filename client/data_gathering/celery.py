d664b37a59145a3a95c5da340aac90f6061c7418
Carlos Cardoso Dias <carlosdias.dev@gmail.com>
2023-07-04 21:06:52 +0000

    change from Worker to app.start

--- client/data_gathering/celery.py
+++ client/data_gathering/celery.py
@@ -59,6 +59,5 @@ if __name__ == '__main__':
     scheduler.start()
     logger.info('Scheduler started.')
     logger.info('Running celery application...')
-    worker = app.Worker()
-    worker.start()
+    app.start()
     logger.info('Done.')
